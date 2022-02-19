d689b66930c8a357a829cab29146173d48cdd87e
jreese8 <johnathanreese8@gmail.com>
2022-02-19 17:02:15 -0500

    Removed code that wasn't being used in server and changed * to / in index.js in htmlRoutes to make app work

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-const fs = require('fs');
-const path = require('path');
-
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
